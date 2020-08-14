fcd08d97c8130c6dfd85e03086f84af24ad49223
eradicador <janisepinto@yahoo.com>
2020-08-14 02:05:17 -0400

    Updates

---
+++ precache-manifest.e2ee29293731fe4525afdf581c48cf66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447406d746dad9a7dedc19f50aba7e9a",
+    "url": "/React-Responsive-Portfolio/index.html"
+  },
+  {
+    "revision": "3c2c3090a00cf34f1163",
+    "url": "/React-Responsive-Portfolio/static/css/main.7a56e844.chunk.css"
+  },
+  {
+    "revision": "5388533a5360f6372ba1",
+    "url": "/React-Responsive-Portfolio/static/js/2.dd4bfb36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Responsive-Portfolio/static/js/2.dd4bfb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2c3090a00cf34f1163",
+    "url": "/React-Responsive-Portfolio/static/js/main.1052d1d1.chunk.js"
+  },
+  {
+    "revision": "b6a213bbae1c3ad85039",
+    "url": "/React-Responsive-Portfolio/static/js/runtime-main.6868dcd2.js"
+  },
+  {
+    "revision": "8269fa01539dd5f05a6081329a444486",
+    "url": "/React-Responsive-Portfolio/static/media/crossline-lines.8269fa01.png"
+  }
+]);
