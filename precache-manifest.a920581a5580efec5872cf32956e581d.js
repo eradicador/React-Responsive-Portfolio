d0d7b6b69e3c3a9d5df6f6e462b96e00b579b667
eradicador <janisepinto@yahoo.com>
2020-08-16 05:49:01 -0400

    Updates

---
+++ precache-manifest.a920581a5580efec5872cf32956e581d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9312c4e745fa7530c440ef7569853c8",
+    "url": "/React-Responsive-Portfolio/index.html"
+  },
+  {
+    "revision": "1e707a081866bf55525a",
+    "url": "/React-Responsive-Portfolio/static/css/main.7a56e844.chunk.css"
+  },
+  {
+    "revision": "8ebb39b64711cd30a966",
+    "url": "/React-Responsive-Portfolio/static/js/2.6d0f7347.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Responsive-Portfolio/static/js/2.6d0f7347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e707a081866bf55525a",
+    "url": "/React-Responsive-Portfolio/static/js/main.616aaa1b.chunk.js"
+  },
+  {
+    "revision": "b6a213bbae1c3ad85039",
+    "url": "/React-Responsive-Portfolio/static/js/runtime-main.6868dcd2.js"
+  },
+  {
+    "revision": "d5695d3c22bb410492600527b976a122",
+    "url": "/React-Responsive-Portfolio/static/media/Michael_Resume_2020.d5695d3c.pdf"
+  },
+  {
+    "revision": "621b31c35dcd18094bb8057a91227b21",
+    "url": "/React-Responsive-Portfolio/static/media/Professional_2020.621b31c3.jpg"
+  },
+  {
+    "revision": "b5f6bdfaf1e405824997a5b13f58374f",
+    "url": "/React-Responsive-Portfolio/static/media/Unheard_Stories.b5f6bdfa.png"
+  },
+  {
+    "revision": "8269fa01539dd5f05a6081329a444486",
+    "url": "/React-Responsive-Portfolio/static/media/crossline-lines.8269fa01.png"
+  },
+  {
+    "revision": "bcaf7a4c0d810aafc834bae5583504fb",
+    "url": "/React-Responsive-Portfolio/static/media/eat da burger.bcaf7a4c.png"
+  },
+  {
+    "revision": "8ab837b9939e87cd914bfaafa1496982",
+    "url": "/React-Responsive-Portfolio/static/media/streamjunkie.8ab837b9.png"
+  },
+  {
+    "revision": "8f0b64b87c59268bea33cc3a97487fed",
+    "url": "/React-Responsive-Portfolio/static/media/work day scheduler.8f0b64b8.png"
+  }
+]);
